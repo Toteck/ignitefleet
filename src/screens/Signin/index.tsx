338108bec595d5806b506c6bfe0c00def42e9a81
mateus <mateusweslley14@gmail.com>
2025-03-03 01:13:59 -0300

    Add Signin screen and update app configuration
    - Implement Signin screen with styles
    - Update App component to render Signin
    - Add theme configuration
    - Modify app.json for dark mode and asset updates
    - Remove unused splash icon

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export function Signin() {
+  return (
+    <View style={styles.container}>
+      <Text>Signin</Text>
+    </View>
+  );
+}
